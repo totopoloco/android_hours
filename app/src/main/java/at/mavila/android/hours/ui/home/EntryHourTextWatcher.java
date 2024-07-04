24909ef90dd2710b19cd9ce61ea6c8f28a166a37
Marco Tulio Ávila Cerón <marcotavilac@pm.me>
2024-07-04 12:11:49 +0200

    none Takes advantage of Lombok

--- app/src/main/java/at/mavila/android/hours/ui/home/EntryHourTextWatcher.java
+++ app/src/main/java/at/mavila/android/hours/ui/home/EntryHourTextWatcher.java
@@ -6,25 +6,30 @@ import android.util.Log;
 import androidx.fragment.app.Fragment;
 import at.mavila.android.hours.R;
 import at.mavila.android.hours.databinding.FragmentHomeBinding;
+import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 
+@AllArgsConstructor
 public class EntryHourTextWatcher implements TextWatcher {
 
   private final FragmentHomeBinding binding;
   private final HomeViewModel homeViewModel;
   private final Fragment fragment;
 
-  public EntryHourTextWatcher(final FragmentHomeBinding binding, final HomeViewModel homeViewModel, final Fragment fragment) {
-    this.binding = binding;
-    this.homeViewModel = homeViewModel;
-    this.fragment = fragment;
-  }
 
   @Override
   public void beforeTextChanged(CharSequence s, int start, int count, int after) {
     Log.d("HomeFragment", "beforeTextChanged");
   }
 
+  /**
+   * Validates the input and sets the value in the view model.
+   *
+   * @param charSequence The text that was changed
+   * @param start        The start position of the text that was changed
+   * @param before       The length of the text that was replaced
+   * @param count        The length of the new text
+   */
   @Override
   public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
 
--- app/src/main/java/at/mavila/android/hours/ui/home/EntryStartLunchTextWatcher.java
+++ app/src/main/java/at/mavila/android/hours/ui/home/EntryStartLunchTextWatcher.java
@@ -3,12 +3,14 @@ package at.mavila.android.hours.ui.home;
 import android.text.Editable;
 import android.text.TextWatcher;
 import at.mavila.android.hours.databinding.FragmentHomeBinding;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class EntryStartLunchTextWatcher implements TextWatcher {
-  public EntryStartLunchTextWatcher(final FragmentHomeBinding binding,
-                                    final HomeViewModel homeViewModel,
-                                    final HomeFragment homeFragment) {
-  }
+
+  private final FragmentHomeBinding binding;
+  private final HomeViewModel homeViewModel;
+  private final HomeFragment homeFragment;
 
   @Override
   public void beforeTextChanged(CharSequence s, int start, int count, int after) {
