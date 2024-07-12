5c61e2defe630509f32ff1da74ee95aa2233d8e0
Marco Tulio Ávila Cerón <marcotavilac@pm.me>
2024-07-12 19:15:26 +0200

    none struggling with the caret after update

--- app/src/main/java/at/mavila/android/hours/ui/settings/TextInputWatcher.java
+++ app/src/main/java/at/mavila/android/hours/ui/settings/TextInputWatcher.java
@@ -2,7 +2,6 @@ package at.mavila.android.hours.ui.settings;
 
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import com.google.android.material.textfield.TextInputEditText;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -25,20 +24,19 @@ public class TextInputWatcher implements TextWatcher, OnSettingsChangeListener {
   @NonNull
   private final TextInputEditText editText;
 
-  private String previousText;
-  private int previousCursorPosition;
+  private boolean isUpdating = false;
+  private String previousText = "";
+  private int previousCaretPosition = 0;
 
   @Override
   public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-    Log.d("TextInputWatcher",
-        "beforeTextChanged: " + s.toString() + " start: " + start + " count: " + count + " after: " + after);
     this.previousText = s.toString();
-    this.previousCursorPosition = start;
+    this.previousCaretPosition = editText.getSelectionStart();
   }
 
   @Override
   public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+    // No action needed here
   }
 
   /**
@@ -49,50 +47,31 @@ public class TextInputWatcher implements TextWatcher, OnSettingsChangeListener {
    */
   @Override
   public void afterTextChanged(final Editable s) {
-
-    // Get the new value
-    final String newValue = s.toString();
-    // Get the current text
-    final String currentText = this.previousText;
-
-    // Capture the current cursor position
-    final int cursorPosition = this.previousCursorPosition;
-
-    // Check if the text has actually changed
-    if (hasTextActuallyChanged(currentText, newValue)) {
-      // Update the settings
-      onSettingsChanged(newValue);
-      // Since we're setting the text anew, we need to restore the cursor position
-      this.editText.setText(newValue);
-      this.editText.setSelection(Math.min(cursorPosition, newValue.length()));
+    if (this.isUpdating) {
       return;
     }
 
-    tryToRestoreCursorPosition(newValue);
-  }
-
-  /**
-   * Tries to restore the cursor position after the text has been set anew.
-   * It catches any exceptions that might occur.
-   *
-   * @param value The new value of the text
-   */
-  private void tryToRestoreCursorPosition(String value) {
-    // If the text hasn't changed, try to restore the cursor position
-    try {
-      this.editText.setSelection(value.length());
-    } catch (Exception e) {
-      Log.e("TextInputWatcher", "Error setting cursor position", e);
+    if (StringUtils.isBlank(this.previousText)) {
+      this.isUpdating = false;
+      return;
     }
+
+    final String newValue = s.toString();
+    if (valuesHaveChanged(newValue)) {
+      this.isUpdating = true;
+      settingsViewModel.updateSettings(newValue, this.settingsField);
+      // Calculate expected new caret position based on text length change
+      editText.setSelection(newValue.length());
+      this.isUpdating = false;
+      return;
+    }
+    editText.setSelection(previousCaretPosition);
   }
 
-  private static boolean hasTextActuallyChanged(String currentText, String newValue) {
-    return isNotEqualsCurrentAndNew(currentText, newValue) && StringUtils.isNotBlank(currentText);
+  private boolean valuesHaveChanged(String newValue) {
+    return !StringUtils.equals(this.previousText, newValue);
   }
 
-  private static boolean isNotEqualsCurrentAndNew(String currentText, String newValue) {
-    return !StringUtils.equals(currentText, newValue);
-  }
 
   @Override
   public void onSettingsChanged(String newValue) {
